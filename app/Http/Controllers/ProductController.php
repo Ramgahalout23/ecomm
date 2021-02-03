fbf62baa8548e8778e125f2e0060b17cec6acc39
ramkumar <ramkumargahalout67@gmail.com>
2021-02-03 22:28:13 +0530

    code update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Session;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+use Illuminate\Contracts\Session\Session as SessionSession;
+use PhpParser\Node\Expr\New_;
+
 class ProductController extends Controller
 {
-    function index(){
+    function index(){   
         $data= Product::all();
 
         return view('product',['products'=>$data]);
@@ -23,5 +27,24 @@ class ProductController extends Controller
         where('name', 'like', '%'.$req->input('query').'%')
         ->get();    
         return view('search',['products'=>$data]);
+    }
+    function addToCart(Request $req){
+  
+   if($req->session()->has('user')){
+
+    $cart= new Cart;
+            $cart->user_id=$req->session()->get('user')['id'];
+            $cart->product_id=$req->product_id;
+            $cart->save();
+            return redirect('/');
+   }else{
+       return redirect('/login');
+   }
+}
+
+static function cartItem()
+    {
+        $userId=Session::get('user')['id'];
+        return Cart::where('user_id',$userId)->count();
     }
 }
